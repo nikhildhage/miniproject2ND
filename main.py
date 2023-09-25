ebbc8d1fb34834b8ed8749b24c299635d3823eff
nikhildhage <nikhil.dhage@gmail.com>
2023-09-24 22:31:43 -0700

    Retrieved Dataset. Read ub Dataset from json file

--- main.py
+++ main.py
@@ -18,16 +18,55 @@
 # the project should save these when it executes. You may want to add this folder to your .gitignore file.
 # (10/10 points) There should be a minimum of 5 commits on your project, be sure to commit often!
 # (10/10 points) I will be checking out the master branch of your project. Please be sure to include a requirements.txt
-# file which contains all the packages that need installed. You can create this fille with the output of pip freeze at
+# file which contains all the packages that need installed. You can create this file with the output of pip freeze at
 # the terminal prompt.
 # (20/20 points) There should be a README.md file in your project that explains what your project is, how to install the
 # pip requirements, and how to execute the program. Please use the GitHub flavor of Markdown. Be thorough on the
 # explanations.
 
+# Research question : What is the inflation rate for each country aat specific time period ?
+
 import matplotlib.pyplot as plt
+import requests
+import pandas as pd
+import pprint
 import numpy as np
 from pathlib import Path
 
 
+# Takes an api key and gets inflation data for a specific country
+def getInflationData(country):
+    Countries = ["United States", "AAPL", "GME", "SONY", "META"]
+    data = pd.read_json("GlobalDatasetofInflation.json", orient="Country")
+    print(data.head(5))
 
 
+def showGraphs():
+    # stocks list
+    Countries = ["MSFT", "AAPL", "GME", "SONY", "META"]
+
+    # Lop through all countries in Countries Array
+    for country in Countries:
+        # empty numpy array created from returned closing price list
+        countryInflation = np.array(getInflationData(country))
+
+    # Plot the graphs
+    plt.plot(countryInflation)
+
+    # set x and y axis min and max
+    plt.axis([1, 10])
+
+    # Setup graph title and labels
+    plt.title("Inflation for" + " " + "country")
+    plt.xlabel("Countries")
+    plt.ylabel("Data")
+
+    # save Graphs to charts folder
+    saveFile = '''"charts/" + "Country + ".png"'''
+    plt.savefig(saveFile)
+
+    # Show graphs
+    plt.show()
+
+
+getInflationData("United States")
